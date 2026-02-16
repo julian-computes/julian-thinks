f316274387f151d708fd25165fae7035bdfb789a
Julian Fernandez <jufer002.work@gmail.com>
2026-02-16 16:49:34 -0500

    switch to absolute imports

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,7 +1,7 @@
 import { getCollection } from "astro:content";
 import rss from "@astrojs/rss";
-import { SITE_DESCRIPTION, SITE_TITLE } from "../consts";
-import { pubDateFromId } from "../lib/blog";
+import { SITE_DESCRIPTION, SITE_TITLE } from "@/consts";
+import { pubDateFromId } from "@/lib/blog";
 
 export async function GET(context) {
   const posts = await getCollection("blog");
