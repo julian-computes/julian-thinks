eddb9a13fd44c594f6a1dab53b0a4e60b9a9fabc
Julian Fernandez <jufer002.work@gmail.com>
2026-02-16 16:16:38 -0500

    tighten CSP and fix nested HTML

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Julian Thinks';
-export const SITE_DESCRIPTION = 'Julian Fernandez';
+export const SITE_DESCRIPTION = 'Writing by Julian Fernandez';
